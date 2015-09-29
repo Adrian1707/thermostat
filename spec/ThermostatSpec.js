2323e3243ab8eae7e7bfe126535965a1475715be
Adrian Booth <Adrian.booth17@gmail.com>
2015-09-29 18:05:03 +0100

    Finished reset test. All test passing

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,49 @@
+describe('Thermostat', function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('should begin with a temperature of 20 degrees', function() {
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+  it('should increase temperature when up button is pressed', function() {
+    thermostat.upButton();
+    expect(thermostat.temperature).toEqual(21);
+  });
+
+  it('should decrease temperature when down button is pressed', function() {
+    thermostat.downButton();
+    expect(thermostat.temperature).toEqual(19);
+  });
+
+  it('should not drop below 10 degrees', function() {
+    for (i = 0; i < 11; i++) {
+      thermostat.downButton();
+    }
+    expect(function() {thermostat.downButton(); }).toThrow("Temperature cannot be set below 10 degrees");
+  });
+
+  it('should have a maximum temperature of 25 degrees when power save mode is on', function() {
+    for (i = 0; i < 5; i++) {
+    thermostat.upButton();
+    }
+    expect(function() { thermostat.upButton(); }).toThrow("Temperature cannot be set above 25 degrees when power saving mode is on");
+  });
+
+  it('should have a maximum temperature of 32 degrees when power save mode is off', function() {
+    thermostat.powerSaveOff();
+    for (i = 0; i < 12; i++) {
+      thermostat.upButton();
+    }
+    expect(function() { thermostat.upButton(); }).toThrow("Temperature cannot be set above 32 degrees when power saving mode is off");
+  });
+
+  it('can reset the temperature to 20 by hitting the reset button', function() {
+    thermostat.resetButton();
+    expect(thermostat.temperature).toEqual(20)
+  });
+
+});
---
+++ src/Thermostat.js
@@ -0,0 +1,32 @@
+function Thermostat() {
+  this.temperature = 20;
+  this.tempmin = 10;
+  this.powerSave = 'ON';
+  this.powerSaveMaxOn = 25;
+  this.powerSaveMaxOff = 32;
+}
+
+Thermostat.prototype.upButton = function(){
+  if (this.powerSave == 'ON'  && this.temperature >= this.powerSaveMaxOn) {
+    throw 'Temperature cannot be set above 25 degrees when power saving mode is on';
+  }
+  if (this.powerSave == 'OFF' && this.temperature >= this.powerSaveMaxOff) {
+    throw 'Temperature cannot be set above 32 degrees when power saving mode is off';
+  }
+  return this.temperature += 1;
+};
+
+Thermostat.prototype.downButton = function() {
+  if (this.temperature < this.tempmin) {
+    throw 'Temperature cannot be set below 10 degrees';
+  }
+  this.temperature -= 1;
+};
+
+Thermostat.prototype.powerSaveOff = function() {
+  this.powerSave = "OFF";
+};
+
+Thermostat.prototype.resetButton = function() {
+  this.temperature = 20;
+};
