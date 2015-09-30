e9749e1ca31adf1a94abb3d46fa58202f99e3bf6
Adrian Booth <Adrian.booth17@gmail.com>
2015-09-30 17:15:29 +0100

    jQuery added to project in app.js file

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// $( "#btnUp" ).click(function() {
-//  upButton( $( "span:first" ));
-// });
-//
-// $( "#btnDown" ).click(function() {
-//  downButton( $( "span:first" ) );
-// });
-//
-// $( "#btnReset" ).click(function() {
-//  resetButton( $( "span:first" ) );
-// });
-//
-// function upButton( j ) {
-//  var n = parseInt( j.text());
-//  j.text( n + 1 );
-// }
-//
-// function downButton( j ) {
-//  var n = parseInt( j.text());
-//  j.text( n - 1 );
-// }
-//
-// function resetButton( j ) {
-//  var n = parseInt( j.text(20));
-// }
+var thermostat = new Thermostat();
+
+$(document).ready(function(){
+
+  $("#upButton").click(function(){
+    thermostat.upButton();
+  });
+
+  $("#downButton").click(function(){
+    thermostat.downButton();
+  });
+
+  $("#resetButton").click(function(){
+    thermostat.resetButton();
+  });
+
+  $("#powerSaveOn").click(function(){
+    thermostat.powerSaveOn();
+  });
+
+  $("#powerSaveOff").click(function(){
+    thermostat.powerSaveOff();
+  });
+
+});
