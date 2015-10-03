227059de0ec19c79d52e1d90f457e564d9b60950
Adrian Booth <Adrian.booth17@gmail.com>
2015-10-03 15:31:10 +0100

    Add google maps API

--- app.js
+++ app.js
@@ -1,48 +1,50 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   thermostat = new Thermostat;
 
-  $("#upButton").click(function(){
+  $("#upButton").click(function() {
     thermostat.upButton();
     $('#temp').text(thermostat.temperature);
   });
 
-  $("#downButton").click(function(){
+  $("#downButton").click(function() {
     thermostat.downButton();
     $('#temp').text(thermostat.temperature);
   });
 
-  $("#resetButton").click(function(){
+  $("#resetButton").click(function() {
     thermostat.resetButton();
     $('#temp').text(thermostat.temperature);
   });
 
-  $("#powerSaveOn").click(function(){
+  $("#powerSaveOn").click(function() {
     thermostat.powerSaveOn();
     $('#temp').text(thermostat.temperature);
   });
 
-  $("#powerSaveOff").click(function(){
+  $("#powerSaveOff").click(function() {
     thermostat.powerSaveOff();
     $('#temp').text(thermostat.temperature);
   });
 
 
-function fireAjax(data) {
-  $.ajax({
-   url: data.url,
-   success: function(data){
-   $('#city').html("Current city: " + (data.name));
-   $('#weather_temp').html('Current Temperature    ' + (data.main.temp - 273).toFixed(1));
-   $('#weather_humid').html("Humidity is " + (data.main.humidity));
+  function fireAjax(data) {
+    $.ajax({
+      url: data.url,
+      success: function(data) {
+        $('#city').html("Current city: " + (data.name));
+        $('#weather_temp').html('Current Temperature    ' + (data.main.temp - 273).toFixed(1));
+        $('#weather_humid').html("Humidity is " + (data.main.humidity));
+      }
+    });
   }
- });
-}
 
-$("#weather_city").submit(function(event) {
-  var newURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + $('#city_weather').val();
-  fireAjax({url:newURL});
-  event.preventDefault();
-});
+  $("#weather_city").submit(function(event) {
+    var newURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + $('#city_weather').val();
+    fireAjax({
+      url: newURL
+    });
+    event.preventDefault();
+  });
 
 });
