22202f324df0f94883e8f87b9861132a97800cc0
Adrian Booth <Adrian.booth17@gmail.com>
2015-10-03 16:03:49 +0100

    Add API key for geocoder to work

--- src/Maps.js
+++ src/Maps.js
@@ -1,10 +1,26 @@
-function initialize() {
-  var mapCanvas = document.getElementById('map');
-  var mapOptions = {
-    center: new google.maps.LatLng(51.4353, -0.1098),
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(mapCanvas, mapOptions)
+    center: {lat: -34.397, lng: 150.644}
+  });
+  var geocoder = new google.maps.Geocoder();
+
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
